6f95971560c10431c2bbc525e773c1f5de5b261e
Mikeacino <32168280+Mikeacino@users.noreply.github.com>
2019-11-07 16:13:03 -0500

    Update Rides.java
    Added more fields

--- src/sample/Rides.java
+++ src/sample/Rides.java
@@ -1,60 +1,133 @@
 /***********************************************************
- * File   : Account.Java
- * Author(s)  : Michael Carracino, Benjamin Cano
+ * File   : Rides.Java
+ * Author(s)  : Michael Carracino
  * Class   : CEN 3031
- * Purpose : Stores the information from the database for a given Ride
+ * Purpose : Stores the user data pulled from the database
  ************************************************************/
 package sample;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Random;
+public class Account {
 
-public class Rides {
-  private String passengerName;
-  private LocalTime time_OfRide;
-  private LocalDate date_OfRide;
-  private String startLocation;
-  private String endLocation;
-  private int rideID;
-  private String driver;
+  private  static String firstName, lastName, phone, username, password, email, dateOfBirth;
+  private static int userID, carOneID, carTwoID;
+  // static variable of a current user's account, for the sake of updating account details for now
 
-  public Rides(String passName, LocalTime time, LocalDate Date, String startLoc, String endLoc) {
-    this.passengerName = passName;
-    this.date_OfRide = Date;
-    this.time_OfRide = time;
-    this.startLocation = startLoc;
-    this.endLocation = endLoc;
-    Random rnd = new Random();
-    this.rideID = rnd.nextInt(999999);
-    this.driver = "None";
+
+  public Account(int userID, String username, String password,String firstName, String lastName, String email, String phone, String dateOfBirth,int carOneID,int carTwoID) {
+    this.userID = userID;
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.email = email;
+    this.dateOfBirth = dateOfBirth;
+    this.carOneID = carOneID;
+    this.carTwoID = carTwoID;
   }
 
-  public String getPassengerName() {
-    return passengerName;
+
+  /*** ACCESSOR METHODS ***/
+  public String getFirstName() {
+    return firstName;
   }
 
-  public LocalDate getDate_OfRide() {
-    return date_OfRide;
+  public String getLastName() {
+    return lastName;
   }
 
-  public LocalTime getTime_OfRide() {
-    return time_OfRide;
+  public String getEmail() {
+    return email;
   }
 
-  public String getEndLocation() {
-    return endLocation;
+  public String getPhone() {
+    return phone;
   }
 
-  public String getStartLocation() {
-    return startLocation;
+  public String getDateOfBirth() {
+    return dateOfBirth;
   }
 
-  public int getRideID() {
-    return rideID;
+  public String getUsername() {
+    return username;
   }
 
-  public String getDriver() {
-    return driver;
+  public String getPassword() {
+    return password;
+  }
+
+  /*** SETTER METHODS ***/
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setCarTwoID(int carTwoID) {
+    this.carTwoID = carTwoID;
+  }
+
+  public void setCarOneID(int carOneID) {
+    this.carOneID = carOneID;
+  }
+
+  public void setUser_id(int user_id) {
+    this.userID = user_id;
+  }
+
+  /*** HELPER METHOD ***/
+  public String getName() {
+    return getFirstName() + " " + getLastName();
+  }
+
+  public int getUser_id() {
+    return userID;
+  }
+
+  public int getCarOneID() {
+    return carOneID;
+  }
+
+  public int getCarTwoID() {
+    return carTwoID;
+  }
+
+  /**
+   * A print Account for verifying your account with print statements
+   */
+  public void printAccountDetails(){
+    System.out.println(""
+        + "\nUserID:  "+userID
+        + "\nUsername:  "+username
+        + "\nPassword:  "+password
+        + "\nFirstName:  "+firstName
+        + "\nLastName:  "+lastName
+        + "\nPhone:  "+phone
+        + "\nEmail:  "+email
+        + "\nDateOfBirth:  "+dateOfBirth
+        + "\nCarOneID:  "+carOneID
+        + "\nCarTwoID:  "+carTwoID);
   }
 }
