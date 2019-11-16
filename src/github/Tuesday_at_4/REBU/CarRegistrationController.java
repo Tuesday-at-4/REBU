cc0ae0588ead1cfe68c42f16cf4a1a2e11652579
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-11-15 19:48:37 -0500

    Update CarRegistrationController.java

--- src/github/Tuesday_at_4/REBU/CarRegistrationController.java
+++ src/github/Tuesday_at_4/REBU/CarRegistrationController.java
@@ -7,57 +7,48 @@
  * Purpose : Populates the tables of regsitered vehicles of the user.
  * **********************************************************
  */
-package github.Tuesday_at_4.REBU;
+package sample;
 
-
-import java.net.URL;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class CarRegistrationController implements Initializable {
-  private static String JDBC_DRIVER = "org.h2.Driver";
-  private static String DB_URL = "jdbc:h2:./res/RebuDB";
-  private static String PASS = "";
-  private static String USER = "";
+public class CarRegistrationController {
+
 
   @FXML
   private TableView<Car> RegisteredVehicles;
 
   @FXML
-  private TableColumn<Car, String> manufacturer;
+  private TableColumn<?,?> manufacturer;
 
   @FXML
-  private TableColumn<Car, String> model;
+  private TableColumn<?,?>  model;
 
   @FXML
-  private TableColumn<Car, String> year;
+  private TableColumn<?,?>  year;
 
   @FXML
-  private TableColumn<Car, String> carType;
+  private TableColumn<?,?>  carType;
 
   @FXML
-  private TableColumn<Car, String> licensePlate;
+  private TableColumn<?,?>  licensePlate;
 
   @FXML
-  private TableColumn<Car, String> CarColor;
+  private TableColumn<?,?>  CarColor;
 
   @FXML
-  private TableColumn<Car, String> NumSeats;
+  private TableColumn<?,?>  NumSeats;
 
   @FXML
   private TextField txtField_Manufacturer;
@@ -94,30 +85,19 @@ public class CarRegistrationController implements Initializable {
   private void goDriverPage(Event event){
     Main.createNewScene(event, "Driver.fxml");
   }
-  private ObservableList<Car> oblist =FXCollections.observableArrayList();
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    try{
-      Class.forName(JDBC_DRIVER);
-      Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
-      ResultSet rs = conn.createStatement().executeQuery("select * from CAR_LIST");
-      while(rs.next()){
-        oblist.add(new Car(rs.getInt(0),rs.getString("Manufacturer"),
-            rs.getString("Model"),rs.getInt("year"),rs.getString("carType")
-            ,rs.getString("licensePlate"),rs.getString("CarColor"),rs.getInt("NumSeats")));
-      }
-    }catch(SQLException | ClassNotFoundException ex){
-      Logger.getLogger(CarRegistrationController.class.getName()).log(Level.SEVERE,null,ex);
-    }
+  @FXML
+public void Add_Button(Event event){
 
-    manufacturer.setCellValueFactory(new PropertyValueFactory<>("Manufacturer"));
-    model.setCellValueFactory(new PropertyValueFactory<>("Model"));
-    licensePlate.setCellValueFactory(new PropertyValueFactory<>("licensePlate"));
-    year.setCellValueFactory(new PropertyValueFactory<>("year"));
-    carType.setCellValueFactory(new PropertyValueFactory<>("carType"));
-    NumSeats.setCellValueFactory(new PropertyValueFactory<>("NumSeats"));
-    CarColor.setCellValueFactory(new PropertyValueFactory<>("CarColor"));
-
-    RegisteredVehicles.setItems(oblist);
+Car vehicle = new Car(0,txtField_Manufacturer.getText(),txtField_Model.getText(),Integer.parseInt(txtField_Year.getText()),
+    txtField_carType.getText(),txtField_licensePlate.getText(),txtField_Color.getText(),Integer.parseInt((txtField_Seats.getText())));
+  RegisteredVehicles.getItems().add(vehicle);
   }
+@FXML
+  public void Delete_Button(){
+  TableView<Car> table = new TableView<>();
+  Delete_Button.setOnAction(e -> {
+    Car selectedItem = table.getSelectionModel().getSelectedItem();
+    table.getItems().remove(selectedItem);
+  });
+}
 }
